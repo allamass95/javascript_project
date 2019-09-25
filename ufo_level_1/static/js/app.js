988bc6dfedd996128bc983457e2e22accdf91df3
Alejandro Llamas <allamass95@gmail.com>
2019-09-25 02:35:30 -0500

    finalized

--- ufo_level_1/static/js/app.js
+++ ufo_level_1/static/js/app.js
@@ -1,31 +1,36 @@
 // from data.js
-var tableData = data;
 
 var button = d3.select('#filter-btn');
 
+
 button.on('click',function(){
 
+    var tableData = data;
     var inputElement=d3.select('.form-control');
-
     var inputValue=inputElement.property('value');
-
     console.log(inputValue)
     console.log(tableData)
+    
+    var earasedData = tableData.html('')
 
     var filterData = tableData.filter(data=>data.datetime===inputValue);
-
     console.log(filterData)
-
+    
+    
     filterData.forEach(function(data) {
         console.log(data);
         var tbody=d3.select('#body-slam')
         var row = tbody.append("tr");
+   
         Object.entries(data).forEach(function([key, value]) {
           console.log(key, value);
           // Append a cell to the row for each value
           // in the weather report object
-          var cell = row.append("td");
+          
+          var cell =row.append("td");
+          
           cell.text(value);
+          
         });
 
   
